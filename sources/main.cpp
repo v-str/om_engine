aa36673ad149bb6f832c5715509f2ff224304d9b
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-10 21:51:52 +0300

    Rename module

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,22 +1,7 @@
 ﻿#include <QApplication>
 
-#include <QWidget>
-
-#include <om_button.h>
-
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
-
-  QWidget widget;
-  widget.setGeometry(600, 400, 600, 400);
-
-  OmWidgets::OmButton* button_ = new OmWidgets::OmButton(&widget);
-  button_->setText("OmButton");
-  button_->setGeometry(50, 25, 100, 30);
-  button_->SetOffsetSide(OmUtility::kRight | OmUtility::kUp);
-  button_->SetOffsetDistance(OmUtility::OffsetDistance(5, 5));
-
-  widget.show();
 
   return app.exec();
 }
