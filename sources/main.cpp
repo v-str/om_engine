28ad84c0182afb694f4321aa3bdf2d08d659aaac
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-10 20:08:56 +0300

    Change main

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,2 +1,13 @@
-﻿#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+﻿#define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
+
+#include <QApplication>
+
+int main(int argc, char** argv) {
+  doctest::Context context;
+  QApplication app(argc, argv);
+  context.applyCommandLine(argc, argv);
+  int result_of_tests = context.run();
+
+  return result_of_tests;
+}
