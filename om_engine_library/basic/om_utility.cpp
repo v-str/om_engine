954dfa735d22cd071b07bba391dcc2b8659132b6
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-10 20:45:19 +0300

    Add getters for structure

--- om_engine_library/basic/om_utility.cpp
+++ om_engine_library/basic/om_utility.cpp
@@ -1,1 +1,13 @@
-#include <om_utility.h>
+﻿#include <om_utility.h>
+
+OmEngine::OffsetDistance::OffsetDistance(unsigned int offset_by_x,
+                                         unsigned int offset_by_y)
+    : offset_by_x_(offset_by_x), offset_by_y_(offset_by_y) {}
+
+unsigned int OmEngine::OffsetDistance::OffSetDistanceX() const {
+  return offset_by_x_;
+}
+
+unsigned int OmEngine::OffsetDistance::OffSetDistanceY() const {
+  return offset_by_y_;
+}
--- om_engine_library/basic/om_utility.h
+++ om_engine_library/basic/om_utility.h
@@ -6,6 +6,13 @@ enum Side { kLeft = 1, kRight = 2, kUp = 4, kDown = 8 };
 
 struct OffsetDistance {
   OffsetDistance(unsigned int offset_by_x, unsigned int offset_by_y);
+
+  unsigned int OffSetDistanceX() const;
+  unsigned int OffSetDistanceY() const;
+
+ private:
+  unsigned int offset_by_x_ = 0;
+  unsigned int offset_by_y_ = 0;
 };
 }
 
