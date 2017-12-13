780578e3374b66788034bc5f36f5784460e51ed3
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-13 18:44:54 +0300

    Rename files

--- om_engine_library/animation/animation.h
+++
@@ -1,7 +0,0 @@
-#ifndef ANIMATION_H
-#define ANIMATION_H
-
-class Animation{
-};
-
-#endif // ANIMATION_H
---
+++ om_engine_library/animation/widget_animator.h
@@ -0,0 +1,9 @@
+#ifndef WIDGET_ANIMATOR_H
+#define WIDGET_ANIMATOR_H
+
+namespace om_animation{
+class WidgetAnimator{
+};
+}
+
+#endif // WIDGET_ANIMATOR_H
