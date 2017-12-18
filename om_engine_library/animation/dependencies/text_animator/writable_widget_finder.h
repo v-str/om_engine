ce261171200310df9634dc24cf5344046f6d1dbd
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-18 20:48:01 +0300

    Add check on writability

--- om_engine_library/animation/dependencies/text_animator/writable_widget_finder.h
+++ om_engine_library/animation/dependencies/text_animator/writable_widget_finder.h
@@ -10,15 +10,19 @@ class WritableWidgetFinder {
  public:
   static QVector<QString> WritableWidgets();
   static void SetWritableWidgets(const QVector<QString>& WritableWidgets);
+  static bool IsWidgetWrirable(const QString& widget_name);
 
  private:
   static QVector<QString> writable_widgets_;
+  static bool is_conversion_acceptable_;
 };
 
 #endif  // WRITABLE_WIDGET_FINDER_H
 
 QVector<QString> WritableWidgetFinder::writable_widgets_ = {
     "QLabel", "QPushButton", "QLineEdit"};
+
+bool WritableWidgetFinder::is_conversion_acceptable_ = false;
 
 QVector<QString> WritableWidgetFinder::WritableWidgets() {
   return writable_widgets_;
@@ -27,4 +31,8 @@ QVector<QString> WritableWidgetFinder::WritableWidgets() {
 void WritableWidgetFinder::SetWritableWidgets(
     const QVector<QString>& writable_widgets) {
   writable_widgets_ = writable_widgets;
+}
+
+bool WritableWidgetFinder::IsWidgetWrirable(const QString& widget_name) {
+  return is_conversion_acceptable_ = writable_widgets_.contains(widget_name);
 }
