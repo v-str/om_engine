37d4057634b83a2a6e1af12cb279d3e4730cc5ff
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-18 11:59:55 +0300

    Rename class Clear

---
+++ om_engine_library/animation/main/text_animator/text_postponer.cpp
@@ -0,0 +1,1 @@
+﻿#include <text_postponer.h>
---
+++ om_engine_library/animation/main/text_animator/text_postponer.h
@@ -0,0 +1,16 @@
+﻿#ifndef TEXT_POSTPONER_H
+#define TEXT_POSTPONER_H
+
+#include <QLabel>
+#include <QObject>
+#include <QString>
+
+class TextPostponer : public QObject {
+  Q_OBJECT
+ public:
+ public slots:
+
+ private:
+};
+
+#endif  // TEXT_POSTPONER_H
