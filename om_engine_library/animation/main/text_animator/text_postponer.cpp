c6148a89a22d028a5fc5d5c5aab3f127b0f19fb6
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-18 13:28:58 +0300

    Add text postponer class

--- om_engine_library/animation/main/text_animator/text_postponer.cpp
+++ om_engine_library/animation/main/text_animator/text_postponer.cpp
@@ -1,1 +1,48 @@
 ﻿#include <text_postponer.h>
+
+TextPostponer::TextPostponer(QObject* parent,
+                             unsigned int interval_postpone_msec)
+    : QObject(parent),
+      interval_postpone_msec_(interval_postpone_msec),
+      timer_(new QTimer(parent)) {
+  connect(timer_, SIGNAL(timeout()), SLOT(Run()));
+}
+
+TextPostponer::~TextPostponer() {}
+
+void TextPostponer::RunTextPosponing(QLabel* postpone_label) {
+  postpone_label_ = postpone_label;
+  timer_->start(interval_postpone_msec_);
+}
+
+void TextPostponer::ResetIntervalPostpone(unsigned int interval_postpone_msec) {
+  interval_postpone_msec_ = interval_postpone_msec;
+}
+
+void TextPostponer::SetPostponeText(const QString& postpone_text) {
+  postpone_text_ = postpone_text;
+}
+
+void TextPostponer::Run() {
+  Reset();
+  SetCurrentText();
+}
+
+void TextPostponer::Reset() {
+  postpone_text_.clear();
+  current_text_.clear();
+  symbol_count_ = 0;
+}
+
+void TextPostponer::SetCurrentText() {
+  current_text_ += postpone_text_[symbol_count_];
+  postpone_label_->setText(current_text_);
+  if (IsStringEnd()) {
+    timer_->stop();
+  }
+  ++symbol_count_;
+}
+
+bool TextPostponer::IsStringEnd() const {
+  return symbol_count_ == (postpone_text_.size() - 1);
+}
--- om_engine_library/animation/main/text_animator/text_postponer.h
+++ om_engine_library/animation/main/text_animator/text_postponer.h
@@ -4,13 +4,36 @@
 #include <QLabel>
 #include <QObject>
 #include <QString>
+#include <QTimer>
 
 class TextPostponer : public QObject {
   Q_OBJECT
  public:
- public slots:
+  explicit TextPostponer(QObject* parent = nullptr,
+                         unsigned int interval_postpone_msec = 10);
+  ~TextPostponer();
+
+  void RunTextPosponing(QLabel* postpone_label);
+  void ResetIntervalPostpone(unsigned int interval_postpone_msec);
+  void SetPostponeText(const QString& postpone_text);
+
+ private slots:
+  void Run();
 
  private:
+  void Reset();
+  void SetCurrentText();
+
+  bool IsStringEnd() const;
+
+  QTimer* timer_ = nullptr;
+  QLabel* postpone_label_ = nullptr;
+
+  QString postpone_text_;
+  QString current_text_;
+
+  unsigned int interval_postpone_msec_;
+  unsigned int symbol_count_ = 0;
 };
 
 #endif  // TEXT_POSTPONER_H
