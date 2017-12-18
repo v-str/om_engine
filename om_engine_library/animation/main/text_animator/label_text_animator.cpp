79402b9158808b5a2a5de6d0c2818e3911875036
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-18 11:45:18 +0300

    Add constructors

--- om_engine_library/animation/main/text_animator/label_text_animator.cpp
+++ om_engine_library/animation/main/text_animator/label_text_animator.cpp
@@ -1,1 +1,17 @@
-#include <label_text_animator.h>
+﻿#include <label_text_animator.h>
+
+LabelTextAnimator::LabelTextAnimator(QObject* parent) : QObject(parent) {}
+
+LabelTextAnimator::LabelTextAnimator(const QString& text,
+                                     bool is_running,
+                                     QObject* parent)
+    : QObject(parent) {
+  animation_text_buffer_ = text;
+  is_running_ = is_running;
+}
+
+LabelTextAnimator::~LabelTextAnimator() {}
+
+void LabelTextAnimator::SetAnimationText(const QString& text) {
+  animation_text_buffer_ = text;
+}
