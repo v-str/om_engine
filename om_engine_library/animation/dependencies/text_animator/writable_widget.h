ba06fd496ef6bb6460dca2b2f689ce24851ed287
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-19 20:34:35 +0300

    Add template class realization For type erasure idiom

--- om_engine_library/animation/dependencies/text_animator/writable_widget.h
+++ om_engine_library/animation/dependencies/text_animator/writable_widget.h
@@ -8,4 +8,18 @@ class AbstractWritableWidget {
   virtual void SetText(const QString& text) = 0;
 };
 
+template <class Widget>
+class WritableWidget : public AbstractWritableWidget {
+ public:
+  WritableWidget(Widget* writable_widget) : writable_widget_(writable_widget) {}
+  ~WritableWidget() {}
+
+  void SetText(const QString& text) override {
+    writable_widget_->setText(text);
+  }
+
+ private:
+  Widget* writable_widget_ = nullptr;
+};
+
 #endif  // WRITABLE_WIDGET_H
