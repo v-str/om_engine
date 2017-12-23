7f86ed4bb2e2124632a1d056b141314423cacd02
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-23 19:17:33 +0300

    Add class for time and date labels Edit code style file

---
+++ om_engine_library/widgets/OmTimeDate/om_td.h
@@ -0,0 +1,18 @@
+﻿// widget for time and date
+//
+
+#ifndef OM_TD_H
+#define OM_TD_H
+
+#include <QLabel>
+#include <QObject>
+
+namespace om_widgets {
+class OmTD : public QObject {
+  Q_OBJECT
+ public:
+ private:
+};
+}
+
+#endif  // OM_TD_H
