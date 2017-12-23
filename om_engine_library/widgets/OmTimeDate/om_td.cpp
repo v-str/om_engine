7fb00262462d2cad2d8e63db2dc9fcda2030b1af
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-23 19:48:33 +0300

    Add ctor and dtor

--- om_engine_library/widgets/OmTimeDate/om_td.cpp
+++ om_engine_library/widgets/OmTimeDate/om_td.cpp
@@ -1,1 +1,13 @@
-#include <om_td.h>
+﻿#include <om_td.h>
+
+#include <QLabel>
+
+using namespace om_widgets;
+
+OmTD::OmTD(QWidget* parent, Qt::Orientation labels_orientation)
+    : QWidget(parent), labels_orientation_(labels_orientation) {
+  time_label_ = new QLabel(this);
+  date_label_ = new QLabel(this);
+}
+
+OmTD::~OmTD() {}
--- om_engine_library/widgets/OmTimeDate/om_td.h
+++ om_engine_library/widgets/OmTimeDate/om_td.h
@@ -1,17 +1,25 @@
-﻿/*OmTd - om time and date widget for time and date
-contain couple of QLabel objects and few custom class to animate them*/
+﻿// OmTd - om time and date widget for time and date
 
 #ifndef OM_TD_H
 #define OM_TD_H
 
-#include <QLabel>
-#include <QObject>
+#include <QWidget>
+
+class QLabel;
 
 namespace om_widgets {
-class OmTD : public QObject {
+class OmTD : public QWidget {
   Q_OBJECT
  public:
+  OmTD(QWidget* parent = nullptr,
+       Qt::Orientation labels_orientation = Qt::Horizontal);
+  ~OmTD();
+
  private:
+  QLabel* time_label_ = nullptr;
+  QLabel* date_label_ = nullptr;
+
+  Qt::Orientation labels_orientation_;
 };
 }
 
