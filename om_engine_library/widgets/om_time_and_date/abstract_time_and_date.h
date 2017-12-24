3d0aca470c5525f045dc9a576958262bf75c45f6
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-24 12:15:51 +0300

    Re-create classes for time and date

---
+++ om_engine_library/widgets/om_time_and_date/abstract_time_and_date.h
@@ -0,0 +1,18 @@
+﻿#ifndef ABSTRACT_TIME_AND_DATE_H
+#define ABSTRACT_TIME_AND_DATE_H
+
+namespace om_widgets {
+
+class AbstractTimeAndDate {
+ public:
+  virtual void Update() = 0;
+
+ protected:
+  unsigned int TimerTick() { return kTimerTick; }
+
+ private:
+  static const unsigned int kTimerTick = 1000;
+};
+}
+
+#endif  // ABSTRACT_TIME_AND_DATE_H
---
+++ om_engine_library/widgets/om_time_and_date/om_date.cpp
@@ -0,0 +1,1 @@
+#include <om_date.h>
