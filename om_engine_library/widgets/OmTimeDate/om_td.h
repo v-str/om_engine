519b966247900b3c97082e08dc6d8627dbc27a3a
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-24 08:05:50 +0300

    Add box layout initialization

--- om_engine_library/widgets/OmTimeDate/om_td.h
+++ om_engine_library/widgets/OmTimeDate/om_td.h
@@ -1,4 +1,4 @@
-﻿// OmTd - om time and date widget for time and date
+﻿// OmTd - om time and date widget for time and date displaying
 
 #ifndef OM_TD_H
 #define OM_TD_H
@@ -6,22 +6,26 @@
 #include <QWidget>
 
 class QLabel;
-class
+class QBoxLayout;
 
-    namespace om_widgets {
-  class OmTD : public QWidget {
-    Q_OBJECT
-   public:
-    OmTD(QWidget* parent = nullptr,
-         Qt::Orientation labels_orientation = Qt::Horizontal);
-    ~OmTD();
+namespace om_widgets {
+class OmTD : public QWidget {
+  Q_OBJECT
+ public:
+  OmTD(QWidget* parent = nullptr,
+       Qt::Orientation labels_orientation = Qt::Horizontal);
+  ~OmTD();
 
-   private:
-    QLabel* time_label_ = nullptr;
-    QLabel* date_label_ = nullptr;
+ private:
+  void InitializeLayout();
 
-    Qt::Orientation labels_orientation_;
-  };
+  QLabel* time_label_ = nullptr;
+  QLabel* date_label_ = nullptr;
+
+  QBoxLayout* box_layout_ = nullptr;
+
+  Qt::Orientation labels_orientation_;
+};
 }
 
 #endif  // OM_TD_H
