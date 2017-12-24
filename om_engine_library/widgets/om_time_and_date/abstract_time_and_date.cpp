d531d75782ea7c43029eeec99b3f3989c1d9ff66
OrdinaryMind <1.ordinarymind@gmail.com>
2017-12-24 13:26:26 +0300

    Add translation unit for abstract class

---
+++ om_engine_library/widgets/om_time_and_date/abstract_time_and_date.cpp
@@ -0,0 +1,14 @@
+﻿#include <abstract_time_and_date.h>
+
+om_widgets::AbstractTimeAndDate::AbstractTimeAndDate(QLabel* parent)
+    : QLabel(parent) {
+  setAlignment(Qt::AlignCenter);
+}
+
+om_widgets::AbstractTimeAndDate::~AbstractTimeAndDate() {}
+
+unsigned int om_widgets::AbstractTimeAndDate::TimerTick() { return kTimerTick; }
+
+void om_widgets::AbstractTimeAndDate::InitializeTimer(QLabel* label) {
+  timer_ = new QTimer(label);
+}
--- om_engine_library/widgets/om_time_and_date/abstract_time_and_date.h
+++ om_engine_library/widgets/om_time_and_date/abstract_time_and_date.h
@@ -8,13 +8,14 @@ namespace om_widgets {
 
 class AbstractTimeAndDate : public QLabel {
  public:
-  AbstractTimeAndDate(QLabel* parent = nullptr) : QLabel(parent) {}
-  ~AbstractTimeAndDate() {}
+  AbstractTimeAndDate(QLabel* parent = nullptr);
+  ~AbstractTimeAndDate();
 
   virtual void Update() = 0;
 
  protected:
-  unsigned int TimerTick() { return kTimerTick; }
+  unsigned int TimerTick();
+  void InitializeTimer(QLabel* label);
 
  private:
   QTimer* timer_ = nullptr;
