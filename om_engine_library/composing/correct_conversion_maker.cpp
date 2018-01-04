fdbd11649f7e13b89c6516d0cc8f0cc6b79785ca
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-04 14:38:21 +0300

    Add default ctor for conversion maker

--- om_engine_library/composing/correct_conversion_maker.cpp
+++ om_engine_library/composing/correct_conversion_maker.cpp
@@ -1,1 +1,4 @@
 #include <correct_conversion_maker.h>
+
+om_composing::CorrectConversionMaker::CorrectConversionMaker()
+    : x_axis_value_(0.0), y_axis_value_(0.0) {}
--- om_engine_library/composing/correct_conversion_maker.h
+++ om_engine_library/composing/correct_conversion_maker.h
@@ -4,7 +4,15 @@
 namespace om_composing {
 
 class CorrectConversionMaker {
+ public:
+  CorrectConversionMaker();
 
+ private:
+  static constexpr float kMinValue = 0.0;
+  static constexpr float kMaxValue = 3.0;
+
+  float x_axis_value_;
+  float y_axis_value_;
 };
 }
 
