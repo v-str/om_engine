70fe72a5477e0763233791845ee2ba0cc197082f
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-04 18:05:39 +0300

    Add definition of the class

--- om_engine_library/composing/dependencies/geometry_modifier.h
+++ om_engine_library/composing/dependencies/geometry_modifier.h
@@ -1,11 +1,31 @@
 ﻿#ifndef GEOMETRY_MODIFIER_H
 #define GEOMETRY_MODIFIER_H
 
-namespace om_composer {
+#include <QRect>
+
+#include <conversion_maker.h>
+#include <delta_size.h>
+
+namespace om_composing {
 
 class GeometryModifier {
  public:
+  virtual void ComputeModification(const QRect& initial_position) = 0;
+
+  void SetModificationConversion(
+      const ConversionMaker& modification_converstion) {
+    modification_converstion_ = modification_converstion;
+  }
+
+  void SetDeltaSize(const DeltaSize& delta_size) { delta_size_ = delta_size; }
+
+  void ModifyTo(unsigned int modified_side) { modified_side_ = modified_side; }
+
  private:
+  ConversionMaker modification_converstion_;
+  DeltaSize delta_size_;
+
+  unsigned int modified_side_;
 };
 }
 
