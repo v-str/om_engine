dcc1842d1b6aab9b86cc67e0e1246ed8085d2b02
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-04 15:54:18 +0300

    Remove namespace in translation unit

--- om_engine_library/composing/conversion_maker.cpp
+++ om_engine_library/composing/conversion_maker.cpp
@@ -1,29 +1,31 @@
 #include <conversion_maker.h>
 
-om_composing::ConversionMaker::ConversionMaker()
+using namespace om_composing;
+
+ConversionMaker::ConversionMaker()
     : x_axis_value_(0.0), y_axis_value_(0.0) {}
 
-om_composing::ConversionMaker::ConversionMaker(
+ConversionMaker::ConversionMaker(
     float x_axis_value, float y_axis_value) {
   x_axis_value_ = CheckValue(x_axis_value);
   y_axis_value_ = CheckValue(y_axis_value);
 }
 
-void om_composing::ConversionMaker::SetConversionByX(
+void ConversionMaker::SetConversionByX(
     float x_axis_value) {
   x_axis_value_ = CheckValue(x_axis_value);
 }
 
-void om_composing::ConversionMaker::SetConversionByY(
+void ConversionMaker::SetConversionByY(
     float y_axis_value) {
   y_axis_value_ = CheckValue(y_axis_value);
 }
 
-float om_composing::ConversionMaker::X() const { return x_axis_value_; }
+float ConversionMaker::X() const { return x_axis_value_; }
 
-float om_composing::ConversionMaker::Y() const { return y_axis_value_; }
+float ConversionMaker::Y() const { return y_axis_value_; }
 
-float om_composing::ConversionMaker::CheckValue(float axis_value) const {
+float ConversionMaker::CheckValue(float axis_value) const {
   if (axis_value > kMaxValue) {
     return kMaxValue;
   } else if (axis_value < kMinValue) {
