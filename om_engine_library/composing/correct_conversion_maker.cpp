897d81d9c86379329014d9b0380702b2f242fb35
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-04 15:38:30 +0300

    Fill the class translation unit

--- om_engine_library/composing/correct_conversion_maker.cpp
+++ om_engine_library/composing/correct_conversion_maker.cpp
@@ -5,6 +5,29 @@ om_composing::CorrectConversionMaker::CorrectConversionMaker()
 
 om_composing::CorrectConversionMaker::CorrectConversionMaker(
     float x_axis_value, float y_axis_value) {
-  x_axis_value_ = x_axis_value;
-  y_axis_value_ = y_axis_value;
+  x_axis_value_ = CheckValue(x_axis_value);
+  y_axis_value_ = CheckValue(y_axis_value);
+}
+
+void om_composing::CorrectConversionMaker::SetConversionByX(
+    float x_axis_value) {
+  x_axis_value_ = CheckValue(x_axis_value);
+}
+
+void om_composing::CorrectConversionMaker::SetConversionByY(
+    float y_axis_value) {
+  y_axis_value_ = CheckValue(y_axis_value);
+}
+
+float om_composing::CorrectConversionMaker::X() const { return x_axis_value_; }
+
+float om_composing::CorrectConversionMaker::Y() const { return y_axis_value_; }
+
+float om_composing::CorrectConversionMaker::CheckValue(float axis_value) const {
+  if (axis_value > kMaxValue) {
+    return kMaxValue;
+  } else if (axis_value < kMinValue) {
+    return kMinValue;
+  }
+  return axis_value;
 }
--- om_engine_library/composing/correct_conversion_maker.h
+++ om_engine_library/composing/correct_conversion_maker.h
@@ -8,7 +8,15 @@ class CorrectConversionMaker {
   CorrectConversionMaker();
   CorrectConversionMaker(float x_axis_value, float y_axis_value);
 
+  void SetConversionByX(float x_axis_value);
+  void SetConversionByY(float y_axis_value);
+
+  float X() const;
+  float Y() const;
+
  private:
+  float CheckValue(float axis_value) const;
+
   static constexpr float kMinValue = 0.0;
   static constexpr float kMaxValue = 3.0;
 
