ac0e886c086dd64ff4227ef76e36e5dd6c7b1a85
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-04 16:47:55 +0300

    Add setters and getters for class

--- om_engine_library/composing/shifter.cpp
+++ om_engine_library/composing/shifter.cpp
@@ -1,3 +1,19 @@
 ﻿#include <shifter.h>
 
 using namespace om_composing;
+
+void Shifter::MakeShiftConversion(const ConversionMaker &shift_conversion) {
+  shift_conversion_ = shift_conversion;
+}
+
+void Shifter::SetShiftSide(unsigned int shift_side) {
+  shift_side_ = shift_side;
+}
+
+void Shifter::SetDeltaSize(const DeltaSize &delta_size) {
+  delta_size_ = delta_size;
+}
+
+int Shifter::XPos() const { return x_pos_; }
+
+int Shifter::YPos() const { return y_pos_; }
--- om_engine_library/composing/shifter.h
+++ om_engine_library/composing/shifter.h
@@ -1,11 +1,28 @@
 ﻿#ifndef SHIFTER_H
 #define SHIFTER_H
 
+#include <conversion_maker.h>
+#include <delta_size.h>
+
 namespace om_composing {
 
 class Shifter {
  public:
+  void MakeShiftConversion(const ConversionMaker& shift_conversion);
+  void SetShiftSide(unsigned int shift_side);
+  void SetDeltaSize(const DeltaSize& delta_size);
+
+  int XPos() const;
+  int YPos() const;
+
  private:
+  ConversionMaker shift_conversion_;
+  DeltaSize delta_size_;
+
+  unsigned int shift_side_ = 0;
+
+  int x_pos_ = 0;
+  int y_pos_ = 0;
 };
 }
 
