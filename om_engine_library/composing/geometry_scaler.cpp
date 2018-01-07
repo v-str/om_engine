48ec78c4196010676261c6f5162180f119bd9d6e
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-07 16:48:22 +0300

    Add class for geometry scaling

---
+++ om_engine_library/composing/geometry_scaler.cpp
@@ -0,0 +1,19 @@
+﻿#include <geometry_scaler.h>
+
+#include <shifter.h>
+#include <stretcher.h>
+
+using namespace om_composing;
+
+GeometryScaler::GeometryScaler(
+    const ModificationFactor &modification_shift_factor,
+    const ModificationFactor &modification_stretch_factor, Side shift_to,
+    Side stretch_to) {
+  shifter_ = std::move(std::unique_ptr<GeometryModifier>(new Shifter));
+  shifter_->SetModificationFactor(modification_shift_factor);
+  shifter_->ModifyTo(shift_to);
+
+  stretcher_ = std::move(std::unique_ptr<GeometryModifier>(new Stretcher));
+  stretcher_->SetModificationFactor(modification_stretch_factor);
+  stretcher_->ModifyTo(stretch_to);
+}
---
+++ om_engine_library/composing/geometry_scaler.h
@@ -0,0 +1,28 @@
+﻿#ifndef GEOMETRY_SCALER_H
+#define GEOMETRY_SCALER_H
+
+#include <memory>
+
+#include <QRect>
+#include <QWidget>
+
+#include <geometry_modifier.h>
+#include <om_utility.h>
+
+namespace om_composing {
+using namespace om_utility;
+
+class GeometryScaler {
+ public:
+  GeometryScaler(const ModificationFactor& modification_shift_factor,
+                 const ModificationFactor& modification_stretch_factor,
+                 Side shift_to = kRight,
+                 Side stretch_to = kRight);
+
+ private:
+  std::unique_ptr<GeometryModifier> shifter_;
+  std::unique_ptr<GeometryModifier> stretcher_;
+};
+}
+
+#endif  // GEOMETRY_SCALER_H
