da45d5d674091f2246425443c83be9829e6dbc7c
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-07 20:16:13 +0300

    Add class for common widget geometry modification

---
+++ sources/support/geometry_controller.cpp
@@ -0,0 +1,13 @@
+﻿#include <geometry_controller.h>
+
+using namespace demo_code;
+
+GeometryComposer GeometryController::composer_(ModificationFactor(1.0, 0.0));
+
+GeometryScaler GeometryController::scaler_(ModificationFactor(0.1, 0.1),
+                                           ModificationFactor(0.1, 0.1));
+
+void GeometryController::SetDeltaSize(const DeltaSize &delta_size) {
+  composer_.SetDeltaSize(delta_size);
+  scaler_.SetDeltaSize(delta_size);
+}
---
+++ sources/support/geometry_controller.h
@@ -0,0 +1,21 @@
+﻿#ifndef GEOMETRY_CONTROLLER_H
+#define GEOMETRY_CONTROLLER_H
+
+#include <geometry_composer.h>
+#include <geometry_scaler.h>
+
+namespace demo_code {
+using namespace om_composing;
+using namespace om_utility;
+
+class GeometryController {
+ public:
+  static void SetDeltaSize(const DeltaSize& delta_size);
+
+ private:
+  static GeometryComposer composer_;
+  static GeometryScaler scaler_;
+};
+}
+
+#endif  // GEOMETRY_CONTROLLER_H
