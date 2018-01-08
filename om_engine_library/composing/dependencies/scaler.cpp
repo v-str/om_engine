b66fbd5474e04e4ce5d027744c85fab7ea624b31
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-08 20:10:03 +0300

    Add virtual methods definitions

---
+++ om_engine_library/composing/dependencies/scaler.cpp
@@ -0,0 +1,31 @@
+﻿#include <scaler.h>
+
+#include <QRect>
+
+using namespace om_composing;
+
+Scaler::Scaler(const ModificationFactor &modification_shift_factor,
+               const ModificationFactor &modification_stretch_factor,
+               Side shift_to, Side stretch_to) {
+  Shifter::SetModificationFactor(modification_shift_factor);
+  Shifter::ModifyTo(shift_to);
+
+  Stretcher::SetModificationFactor(modification_stretch_factor);
+  Stretcher::ModifyTo(stretch_to);
+}
+
+void Scaler::ComputeModification(const QRect &initial_position) {
+  Shifter::ComputeModification(initial_position);
+  Stretcher::ComputeModification(initial_position);
+}
+
+QRect Scaler::GetModifiedRect() const {
+  return QRect(Shifter::GetModifiedRect().x(), Shifter::GetModifiedRect().y(),
+               Stretcher::GetModifiedRect().width(),
+               Stretcher::GetModifiedRect().height());
+}
+
+void Scaler::SetDeltaSize(const DeltaSize &delta_size) {
+  Shifter::SetDeltaSize(delta_size);
+  Stretcher::SetDeltaSize(delta_size);
+}
---
+++ om_engine_library/composing/dependencies/scaler.h
@@ -0,0 +1,33 @@
+﻿#ifndef SCALER_H
+#define SCALER_H
+
+#include <memory>
+
+#include <om_utility.h>
+#include <shifter.h>
+#include <stretcher.h>
+
+class QRect;
+
+namespace om_composing {
+using namespace om_utility;
+
+class Scaler : public Shifter, public Stretcher {
+ public:
+  Scaler(const ModificationFactor& modification_shift_factor,
+         const ModificationFactor& modification_stretch_factor,
+         Side shift_to = kRight, Side stretch_to = kRight);
+
+  void ComputeModification(const QRect& initial_position) override;
+
+  QRect GetModifiedRect() const override;
+
+  void SetDeltaSize(const DeltaSize &delta_size) override;
+
+ private:
+  std::unique_ptr<Shifter> shifter_;
+  std::unique_ptr<Stretcher> stretcher_;
+};
+}
+
+#endif  // SCALER_H
