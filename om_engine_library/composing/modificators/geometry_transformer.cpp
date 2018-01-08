bdf14b3b41d7795ab1f48d6c47df56bd6c6e71fe
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-08 19:22:51 +0300

    Add abstract class for all geometry modifications

---
+++ om_engine_library/composing/modificators/geometry_transformer.cpp
@@ -0,0 +1,15 @@
+﻿#include <geometry_transformer.h>
+
+#include <QRect>
+#include <QWidget>
+
+using namespace om_composing;
+
+GeometryTransformer::GeometryTransformer(
+    const ModificationFactor &modification_factor, unsigned int side_to_modify,
+    TransformationType type) {}
+
+GeometryTransformer::GeometryTransformer(
+    const ModificationFactor &modification_shift_factor,
+    const ModificationFactor &modification_stretch_factor, Side shift_to,
+    Side stretch_to) {}
---
+++ om_engine_library/composing/modificators/geometry_transformer.h
@@ -0,0 +1,29 @@
+﻿#ifndef GEOMETRY_TRANSFORMER_H
+#define GEOMETRY_TRANSFORMER_H
+
+#include <memory>
+
+#include <geometry_modifier.h>
+#include <om_utility.h>
+
+class QRect;
+class QWidget;
+
+namespace om_composing {
+using namespace om_utility;
+
+class GeometryTransformer {
+ public:
+  GeometryTransformer(const ModificationFactor& modification_factor,
+                      unsigned int side_to_modify = kRight,
+                      TransformationType type = kShifting);
+
+  GeometryTransformer(const ModificationFactor& modification_shift_factor,
+                      const ModificationFactor& modification_stretch_factor,
+                      Side shift_to = kRight, Side stretch_to = kRight);
+
+ private:
+};
+}
+
+#endif  // GEOMETRY_TRANSFORMER_H
