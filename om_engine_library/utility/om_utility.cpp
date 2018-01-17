b1030b6268e692adbb95b93296325ab73c38ebbb
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-17 21:45:53 +0300

    Add namespaces declaration

--- om_engine_library/utility/om_utility.cpp
+++ om_engine_library/utility/om_utility.cpp
@@ -5,22 +5,20 @@
 #include <QRect>
 #include <QTime>
 
-om_utility::OffsetDistance::OffsetDistance(unsigned int offset_by_x,
-                                           unsigned int offset_by_y)
+using namespace om_utility;
+
+OffsetDistance::OffsetDistance(unsigned int offset_by_x,
+                               unsigned int offset_by_y)
     : offset_by_x_(offset_by_x), offset_by_y_(offset_by_y) {}
 
-unsigned int om_utility::OffsetDistance::OffSetDistanceX() const {
-  return offset_by_x_;
-}
+unsigned int OffsetDistance::OffSetDistanceX() const { return offset_by_x_; }
 
-unsigned int om_utility::OffsetDistance::OffSetDistanceY() const {
-  return offset_by_y_;
-}
+unsigned int OffsetDistance::OffSetDistanceY() const { return offset_by_y_; }
 
-void om_utility::TimeDateChanger::ChangeTime(QLabel* time_label) {
+void TimeDateChanger::ChangeTime(QLabel* time_label) {
   time_label->setText(QTime::currentTime().toString("hh:mm:ss"));
 }
 
-void om_utility::TimeDateChanger::ChangeDate(QLabel* date_label) {
+void TimeDateChanger::ChangeDate(QLabel* date_label) {
   date_label->setText(QDate::currentDate().toString("dd.MM.yy"));
 }
--- om_engine_library/widgets/om_button/om_button.cpp
+++ om_engine_library/widgets/om_button/om_button.cpp
@@ -1,54 +1,54 @@
 ﻿#include <om_button.h>
 
 using namespace om_widgets;
+using namespace om_utility;
 
-om_widgets::OmButton::OmButton(QWidget* parent) : QPushButton(parent) {}
+OmButton::OmButton(QWidget* parent) : QPushButton(parent) {}
 
-om_widgets::OmButton::OmButton(const QString& button_text, QWidget* parent)
+OmButton::OmButton(const QString& button_text, QWidget* parent)
     : QPushButton(button_text, parent) {}
 
-om_widgets::OmButton::~OmButton() {}
+OmButton::~OmButton() {}
 
-void om_widgets::OmButton::SetOffsetSide(unsigned int offset_side) {
+void OmButton::SetOffsetSide(unsigned int offset_side) {
   offset_side_ = offset_side;
 }
 
-void om_widgets::OmButton::SetOffsetDistance(
-    const om_utility::OffsetDistance& offset_distance) {
+void OmButton::SetOffsetDistance(const OffsetDistance& offset_distance) {
   offset_distance_ = offset_distance;
 }
 
-void om_widgets::OmButton::enterEvent(QEvent*) {
+void OmButton::enterEvent(QEvent*) {
   int x_pos = x();
   int y_pos = y();
-  if (offset_side_ & om_utility::kLeft) {
+  if (offset_side_ & kLeft) {
     x_pos -= offset_distance_.OffSetDistanceX();
   }
-  if (offset_side_ & om_utility::kRight) {
+  if (offset_side_ & kRight) {
     x_pos += offset_distance_.OffSetDistanceX();
   }
-  if (offset_side_ & om_utility::kUp) {
+  if (offset_side_ & kUp) {
     y_pos -= offset_distance_.OffSetDistanceY();
   }
-  if (offset_side_ & om_utility::kDown) {
+  if (offset_side_ & kDown) {
     y_pos += offset_distance_.OffSetDistanceY();
   }
   move(x_pos, y_pos);
 }
 
-void om_widgets::OmButton::leaveEvent(QEvent*) {
+void OmButton::leaveEvent(QEvent*) {
   int x_pos = x();
   int y_pos = y();
-  if (offset_side_ & om_utility::kLeft) {
+  if (offset_side_ & kLeft) {
     x_pos += offset_distance_.OffSetDistanceX();
   }
-  if (offset_side_ & om_utility::kRight) {
+  if (offset_side_ & kRight) {
     x_pos -= offset_distance_.OffSetDistanceX();
   }
-  if (offset_side_ & om_utility::kUp) {
+  if (offset_side_ & kUp) {
     y_pos += offset_distance_.OffSetDistanceY();
   }
-  if (offset_side_ & om_utility::kDown) {
+  if (offset_side_ & kDown) {
     y_pos -= offset_distance_.OffSetDistanceY();
   }
   move(x_pos, y_pos);
