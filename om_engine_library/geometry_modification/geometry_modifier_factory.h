ce9869808a09205eb823694215ed4fa97be4a1c5
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-22 11:12:12 +0300

    Add first version of factory

--- om_engine_library/geometry_modification/geometry_modifier_factory.h
+++ om_engine_library/geometry_modification/geometry_modifier_factory.h
@@ -1,6 +1,13 @@
 ﻿#ifndef GEOMETRY_MODIFIER_FACTORY_H
 #define GEOMETRY_MODIFIER_FACTORY_H
 
-class GeometryModifierFactory {};
+#include <geometry_modifier.h>
+#include <om_utility.h>
+
+class GeometryModifierFactory {
+ public:
+  geometry_modification::GeometryModifier* Create(
+      om_utility::TransformationType& type);
+};
 
 #endif  // GEOMETRY_MODIFIER_FACTORY_H
