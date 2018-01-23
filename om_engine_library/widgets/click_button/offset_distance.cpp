7cc3201865309bbcf681c8c9856474d6f3e36bfb
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-23 16:14:50 +0300

    Add struct implementation

--- om_engine_library/widgets/click_button/offset_distance.cpp
+++ om_engine_library/widgets/click_button/offset_distance.cpp
@@ -1,1 +1,11 @@
-#include <offset_distance.h>
+﻿#include <offset_distance.h>
+
+using namespace om_widgets;
+
+OffsetDistance::OffsetDistance(unsigned int offset_by_x,
+                               unsigned int offset_by_y)
+    : offset_by_x_(offset_by_x), offset_by_y_(offset_by_y) {}
+
+unsigned int OffsetDistance::GetOffSetDistanceX() const { return offset_by_x_; }
+
+unsigned int OffsetDistance::GetOffSetDistanceY() const { return offset_by_y_; }
--- om_engine_library/widgets/click_button/offset_distance.h
+++ om_engine_library/widgets/click_button/offset_distance.h
@@ -3,7 +3,16 @@
 
 namespace om_widgets {
 
-struct OffsetDistance {};
+struct OffsetDistance {
+  OffsetDistance(unsigned int offset_by_x = 0, unsigned int offset_by_y = 0);
+
+  unsigned int GetOffSetDistanceX() const;
+  unsigned int GetOffSetDistanceY() const;
+
+ private:
+  unsigned int offset_by_x_;
+  unsigned int offset_by_y_;
+};
 }
 
 #endif  // OFFSET_DISTANCE_H
