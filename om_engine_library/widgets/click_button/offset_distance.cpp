2aaa43706c4a224299a10b1b12e1b5e7bf3bf506
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-23 16:08:20 +0300

    Add struct to keep offset distance

---
+++ om_engine_library/widgets/click_button/offset_distance.cpp
@@ -0,0 +1,1 @@
+#include <offset_distance.h>
---
+++ om_engine_library/widgets/click_button/offset_distance.h
@@ -0,0 +1,9 @@
+﻿#ifndef OFFSET_DISTANCE_H
+#define OFFSET_DISTANCE_H
+
+namespace om_widgets {
+
+struct OffsetDistance {};
+}
+
+#endif  // OFFSET_DISTANCE_H
