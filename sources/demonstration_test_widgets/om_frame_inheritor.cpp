a5e88894521a56c0f917806b8c59a2dcf060e7f3
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-28 00:03:23 +0300

    Move all methods from test frame to inheritor

--- sources/demonstration_test_widgets/om_frame_inheritor.cpp
+++ sources/demonstration_test_widgets/om_frame_inheritor.cpp
@@ -1,7 +1,72 @@
 ﻿#include <om_frame_inheritor.h>
 
+#include <QFont>
+
+#include <test_message.h>
+#include <test_widget_setter.h>
+
+using namespace om_widgets;
+using namespace om_animation;
+using namespace demo_code;
+
 om_widgets::OmFrameInheritor::OmFrameInheritor(QWidget *parent,
                                                bool is_widget_open)
-    : OmFrame(parent, is_widget_open) {}
+    : OmFrame(parent, is_widget_open),
+      test_label_(new QLabel(this)),
+      animator_(new StateAnimator(test_label_, true)),
+      open_button_(new ClickButton("Open frame", this)),
+      close_button_(new ClickButton("Close frame", this)),
+      display_text_button_(new ClickButton("Display text", this)),
+      text_animator1_(new TextAnimator(this, 30)) {
+  SetFrame();
+  SetWidgets();
+  SetLabel();
+  SetLabelAnimation();
+  SetConnections();
+}
 
 om_widgets::OmFrameInheritor::~OmFrameInheritor() {}
+
+void om_widgets::OmFrameInheritor::DisplayText() {
+  text_animator1_->RunAnimation(test_label_);
+}
+
+void om_widgets::OmFrameInheritor::IsAnimationComplete() {
+  TestMessage::WriteTestMessage(
+      "test_label_ text animation test by method "
+      "complete . . . ");
+}
+
+void om_widgets::OmFrameInheritor::SetFrame() {
+  TestWidgetSetter::CustomizeFrame(this, QRect(0, 0, 0, 0));
+  resize(500, 300);
+}
+
+void om_widgets::OmFrameInheritor::SetWidgets() {
+  TestWidgetSetter::CustomizeButton(open_button_, QRect(30, 10, 95, 30));
+  TestWidgetSetter::CustomizeButton(close_button_, QRect(135, 10, 95, 30));
+  TestWidgetSetter::CustomizeButton(display_text_button_,
+                                    QRect(240, 10, 95, 30));
+}
+
+void om_widgets::OmFrameInheritor::SetLabel() {
+  TestWidgetSetter::CustomizeLabel(test_label_, QRect(30, 100, 500, 190));
+  QFont font = test_label_->font();
+  font.setPointSize(20);
+  test_label_->setFont(font);
+}
+
+void om_widgets::OmFrameInheritor::SetLabelAnimation() {
+  animator_->SetAnimation(QEasingCurve::OutCirc, 500, om_animation::kRight,
+                          om_animation::kLeft);
+  animator_->SetCurrentGeometry(test_label_->geometry());
+  text_animator1_->SetAnimationText("Ordinary Mind is greeting you . . .");
+}
+
+void om_widgets::OmFrameInheritor::SetConnections() {
+  connect(close_button_, SIGNAL(clicked(bool)), animator_, SLOT(Close()));
+  connect(open_button_, SIGNAL(clicked(bool)), animator_, SLOT(Open()));
+  connect(display_text_button_, SIGNAL(clicked(bool)), SLOT(DisplayText()));
+  connect(text_animator1_, SIGNAL(TextAnimationComplete()),
+          SLOT(IsAnimationComplete()));
+}
--- sources/demonstration_test_widgets/om_frame_inheritor.h
+++ sources/demonstration_test_widgets/om_frame_inheritor.h
@@ -3,16 +3,42 @@
 
 #include <om_frame.h>
 
+#include <QLabel>
+
+#include <click_button.h>
+#include <state_animator.h>
+#include <text_animator.h>
+
 class QWidget;
 
 namespace om_widgets {
+
+using namespace om_widgets;
+using namespace om_animation;
+
 class OmFrameInheritor : public OmFrame {
   Q_OBJECT
  public:
   OmFrameInheritor(QWidget* parent = nullptr, bool is_widget_open = true);
   ~OmFrameInheritor();
 
+ private slots:
+  void DisplayText();
+  void IsAnimationComplete();
+
  private:
+  void SetFrame();
+  void SetWidgets();
+  void SetLabel();
+  void SetLabelAnimation();
+  void SetConnections();
+
+  ClickButton* open_button_ = nullptr;
+  ClickButton* close_button_ = nullptr;
+  ClickButton* display_text_button_ = nullptr;
+  QLabel* test_label_ = nullptr;
+  StateAnimator* animator_ = nullptr;
+  TextAnimator* text_animator1_ = nullptr;
 };
 }
 
