e0014f5cc3327f264d246711a7e9b602655d53e4
OrdinaryMind <1.ordinarymind@gmail.com>
2018-01-27 21:50:43 +0300

    Begin OmFrame realization

--- om_engine_library/widgets/om_frame/om_frame.cpp
+++ om_engine_library/widgets/om_frame/om_frame.cpp
@@ -1,1 +1,13 @@
 ﻿#include <om_frame.h>
+
+#include <state_animator.h>
+
+using namespace om_widgets;
+using namespace om_animation;
+using namespace geometry_modification;
+
+OmFrame::OmFrame(QWidget *parent, bool is_widget_open)
+    : QFrame(parent),
+      state_animator_(new StateAnimator(this, is_widget_open)) {}
+
+OmFrame::~OmFrame() {}
--- om_engine_library/widgets/om_frame/om_frame.h
+++ om_engine_library/widgets/om_frame/om_frame.h
@@ -1,11 +1,29 @@
 ﻿#ifndef OM_FRAME_H
 #define OM_FRAME_H
 
-namespace om_widgets {
+#include <scaler.h>
+#include <QFrame>
 
-class OmFrame {
+#include <memory>
+
+class QWidget;
+
+namespace om_animation {
+class StateAnimator;
+}
+
+namespace om_widgets {
+using namespace om_animation;
+using namespace geometry_modification;
+
+class OmFrame : public QFrame, public Scaler {
+  Q_OBJECT
  public:
+  OmFrame(QWidget* parent = nullptr, bool is_widget_open = true);
+  ~OmFrame();
+
  private:
+  std::unique_ptr<StateAnimator> state_animator_;
 };
 }
 
