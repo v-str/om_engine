7416fd9541d8a5f0555039b052afa6d7fa0a9119
OrdinaryMind <1.ordinarymind@gmail.com>
2018-02-04 15:35:36 +0300

    Add new folder for new feature

---
+++ om_engine_library/animation/animators/sliding_box/abstract_sliding_box.cpp
@@ -0,0 +1,7 @@
+﻿#include <abstract_sliding_box.h>
+
+#include <QWidget>
+
+using namespace om_animation;
+
+AbstractSlidingBox::AbstractSlidingBox(QWidget *widget) : QObject(widget) {}
---
+++ om_engine_library/animation/animators/sliding_box/abstract_sliding_box.h
@@ -0,0 +1,19 @@
+﻿#ifndef ABSTRACT_SLIDING_BOX_H
+#define ABSTRACT_SLIDING_BOX_H
+
+#include <QObject>
+
+class QWidget;
+
+namespace om_animation {
+
+class AbstractSlidingBox : public QObject {
+  Q_OBJECT
+ public:
+  AbstractSlidingBox(QWidget* widget = nullptr);
+
+ private:
+};
+}
+
+#endif  // ABSTRACT_SLIDING_BOX_H
