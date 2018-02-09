1fc3d5ac2b4995adb0f5b4ad3978f3a3ed6d1b61
OrdinaryMind <1.ordinarymind@gmail.com>
2018-02-09 17:33:58 +0300

    Add initial class declaration

--- om_engine_library/animation/dependencies/sliding_widget_set/animation_value_setter.h
+++ om_engine_library/animation/dependencies/sliding_widget_set/animation_value_setter.h
@@ -1,9 +1,22 @@
 ﻿#ifndef ANIMATION_VALUE_SETTER_H
 #define ANIMATION_VALUE_SETTER_H
 
+#include <QPair>
+#include <QPropertyAnimation>
+#include <QVector>
+#include <QWidget>
+
 namespace om_animation {
 
-class AnimationValueSetter {};
+class AnimationValueSetter {
+ public:
+  typedef QVector<QPair<QWidget*, QPropertyAnimation*>> AnimationSet;
+
+  virtual void SetStartValue(AnimationSet* animation_set) = 0;
+  virtual void SetEndValue(AnimationSet* animation_set) = 0;
+
+ private:
+};
 }
 
 #endif  // ANIMATION_VALUE_SETTER_H
