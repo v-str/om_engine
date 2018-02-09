99e6f9ab2f439a6bfc0d88a749d7612a8b6ab9b8
OrdinaryMind <1.ordinarymind@gmail.com>
2018-02-09 16:40:16 +0300

    Add conditional operator

--- om_engine_library/animation/animators/sliding_widget_set/h_sliding_widget_set.cpp
+++ om_engine_library/animation/animators/sliding_widget_set/h_sliding_widget_set.cpp
@@ -24,26 +24,28 @@ void HSlidingWidgetSet::SetStartAnimationPosition() {
 }
 
 void HSlidingWidgetSet::SetEndAnimationPosition() {
-  unsigned int count = 0;
+  if (slide_direction_ == kFromLeftToRight) {
+    unsigned int count = 0;
 
-  for (auto &pair : *GetAnimationSet()) {
-    if (count == 0) {
-      pair.second->setEndValue(QRect(start_x_position_, pair.first->y(),
-                                     pair.first->width(),
-                                     pair.first->height()));
-    } else {
-      unsigned int x_position = start_x_position_;
-      for (auto it = GetAnimationSet()->begin();
-           it < GetAnimationSet()->begin() + count; ++it) {
-        x_position += it->first->width() + GetMargin();
+    for (auto &pair : *GetAnimationSet()) {
+      if (count == 0) {
+        pair.second->setEndValue(QRect(start_x_position_, pair.first->y(),
+                                       pair.first->width(),
+                                       pair.first->height()));
+      } else {
+        unsigned int x_position = start_x_position_;
+        for (auto it = GetAnimationSet()->begin();
+             it < GetAnimationSet()->begin() + count; ++it) {
+          x_position += it->first->width() + GetMargin();
+        }
+        pair.second->setEndValue(QRect(x_position, pair.first->y(),
+                                       pair.first->width(),
+                                       pair.first->height()));
+        x_position = start_x_position_;
       }
-      pair.second->setEndValue(QRect(x_position, pair.first->y(),
-                                     pair.first->width(),
-                                     pair.first->height()));
-      x_position = start_x_position_;
-    }
 
-    ++count;
+      ++count;
+    }
   }
 }
 
