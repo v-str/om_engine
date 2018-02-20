3becf735d4d4ea7e7093f864019bd3551f987cfb
OrdinaryMind <1.ordinarymind@gmail.com>
2018-02-20 20:06:54 +0300

    Test commit Launch project on another machine

--- client_code_example/main.cpp
+++ client_code_example/main.cpp
@@ -7,7 +7,9 @@ using namespace client_code;
 int main(int argc, char** argv) {
   QApplication app(argc, argv);
   Q_INIT_RESOURCE(om_engine_resources);
+
   MainWidget widget;
   widget.show();
+
   return app.exec();
 }
